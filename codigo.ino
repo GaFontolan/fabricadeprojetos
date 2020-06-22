989aabd832cb5392c0602ccf4fa74aee06fb8f3c
Samuel Araujo <samuelaraujo74@gmail.com>
2020-06-22 17:23:19 -0300

    Codigo comentado

--- codigo.ino
+++ codigo.ino
@@ -24,22 +24,25 @@ int valor_botao;
 int inches = 0;
 
 int cm = 0;
- 
+
+//FUNCAO DO SENSOR ULTRASSONICO
 long readUltrasonicDistance(int triggerPin, int echoPin)
 {
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  pinMode(triggerPin, OUTPUT);  // LIMPA O TRIGGER
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
+
+  // CONFIGURA O PINO TRIGGER COMO HIGH, POR 10 MICROSSEGUNDOS
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
   pinMode(echoPin, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  
+  // LE O PINO ECHO, E RETORNA O TEMPO DO CAMINHO DA ONDE EM MICROSSEGUNDOS
   return pulseIn(echoPin, HIGH);
 }
 
-void Alzheimer(){
+void Idoso(){
   float cm = 0.01723 * readUltrasonicDistance(trigger,echo);
   if (cm > 200){
     digitalWrite(buzzer, HIGH);
@@ -49,7 +52,7 @@ void Alzheimer(){
   }
 }
 
-void Perigo(){
+void Crianca(){
   float cm = 0.01723 * readUltrasonicDistance(trigger,echo);
   if (cm > 20){
     digitalWrite(buzzer, HIGH);
@@ -62,8 +65,7 @@ void Perigo(){
 void setup(){  
     pinMode(buzzer, OUTPUT);
     pinMode(btn_um, INPUT);
-    pinMode(btn_dois, INPUT_PULLUP);
-    Serial.begin(9600);
+    pinMode(btn_dois, INPUT);
   
 }
 
@@ -71,24 +73,26 @@ void loop(){
 
   
   
- if(digitalRead(btn_um)==  0){  // se o botão for pressionado
-  cont_btn1++;  // atribui +1 a variavel cont
-  delay(300); // da um tempo para o pressionamento, senao o arduino endoida
+ if(digitalRead(btn_um)==  0){  // Se a leitura do botão quando for pressionado for igual a 0
+  cont_btn1++;  // Atribui +1 a variavel cont_btn1
+  delay(300); // Da um tempo para a leitura do arduino, senão crasha.
   }
   
-  if(digitalRead(btn_dois)==  0){  // se o botão for pressionado
-  cont_btn2++;  // atribui +1 a variavel cont
-  delay(300); // da um tempo para o pressionamento, senao o arduino endoida
+  if(digitalRead(btn_dois)==  0){  // Se a leitura do botão quando for pressionado for igual a 0
+  cont_btn2++;  // Atribui +1 a variavel cont_btn2
+  delay(300); // Da um tempo para a leitura do arduino, senão crasha.
   }
   
+// LOGICA PARA LIMITAÇÃO DO BOTAO
  if(cont_btn1 == 3){
     cont_btn1 = 1;
    digitalWrite(pino_a, HIGH);
    digitalWrite(pino_b, LOW);
    digitalWrite(pino_c, LOW);
    digitalWrite(pino_d, LOW);
- }   //  deixa o valor somente de 1 a 3
+ }  //	deixa o valor somente de 1 a 3
  
+// LOGICA PARA LIMITAÇÃO DO BOTAO
  if(cont_btn2==3){
     cont_btn2=1;
    digitalWrite(pino_a, HIGH);
@@ -98,10 +102,11 @@ void loop(){
  }   //  deixa o valor somente de 1 a 3
   
   
+
  if(cont_btn1==1){
   cont_btn2 = 0;
    
-  Alzheimer();
+  Idoso();				// NESSA CONDIÇÃO, QUANDO O BTN1 É PRESSIONADO, A FUNÇÃO IDOSO COMEÇA A FUNCIONAR
    
    digitalWrite(pino_a, 1);
    digitalWrite(pino_b, 0);
@@ -111,7 +116,7 @@ void loop(){
  } else if(cont_btn1 == 2){
     digitalWrite(buzzer, LOW);
    
-     digitalWrite(pino_a, 1);
+     digitalWrite(pino_a, 1);		// NESSA CONDIÇÃO, QUANDO O BTN1 É PRESSIONADO, CONTINUA NESSA FUNÇÃO, MAS DESATIVA O BUZZER
      digitalWrite(pino_b, 0);
      digitalWrite(pino_c, 0);
      digitalWrite(pino_d, 0);
@@ -120,7 +125,7 @@ void loop(){
   if(cont_btn2==1){
     cont_btn1 = 0;
     
-  Perigo();
+  Crianca();				// NESSA CONDIÇÃO, QUANDO O BTN2 É PRESSIONADO, A FUNÇÃO IDOSO COMEÇA A FUNCIONAR
    
    digitalWrite(pino_a, 0);
    digitalWrite(pino_b, 1);
@@ -130,7 +135,7 @@ void loop(){
  }else if(cont_btn2==2){
   digitalWrite(buzzer, LOW);
    
-   digitalWrite(pino_a, 0);
+   digitalWrite(pino_a, 0);		// NESSA CONDIÇÃO, QUANDO O BTN2 É PRESSIONADO, CONTINUA NESSA FUNÇÃO, MAS DESATIVA O BUZZER
    digitalWrite(pino_b, 1);
    digitalWrite(pino_c, 0);
    digitalWrite(pino_d, 0);
